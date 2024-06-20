3df1819d4e50287f5f829e9d6965d13bd7594888
yukkooooo <calm.and.clear.02@gmail.com>
2024-06-20 15:51:40 +0900

    追加

--- src/App.tsx
+++ src/App.tsx
@@ -3,138 +3,141 @@ import React, { useState } from 'react';
 import Status from './compornents/Status';
 import './App.css';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
-import Side from './compornents/Side';
 
-
+type Todo = {
+  inputValue: string;
+  id: number;
+  checked: boolean;
+  isEditing: boolean;
+  timestamp: string;
+}
 
 function App() {
   const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState<Todo[]>([]);
 
-  type Todo = {
-    inputValue: string;
-    id: number;
-    checked: boolean;
-  }
-
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    // console.log(e.target.value);
     setInputValue(e.target.value);
   }
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-
     // 新しいToDoを作成
     const newTodo: Todo = {
       inputValue: inputValue,
       id: todos.length,
       checked: false,
+      isEditing: false,
+      timestamp: new Date().toLocaleTimeString(),
     };
 
     setTodos([newTodo, ...todos]);
     setInputValue("");
-
   };
-
 
   const handleEdit = (id: number, inputValue: string) => {
     const newTodos = todos.map((todo) => {
       if (todo.id === id) {
         todo.inputValue = inputValue;
       }
-
       return todo;
-
     });
 
     setTodos(newTodos);
-
   };
 
-  const handleChecked = (id: number, checked: boolean) => {
+  const handleChecked = (id: number) => {
     const newTodos = todos.map((todo) => {
       if (todo.id === id) {
-        todo.checked = !checked
+        todo.checked = !todo.checked;
       }
-
       return todo;
-
     });
 
     setTodos(newTodos);
-
   };
 
-  const handledelete = (id: number) => {
+  const handleDelete = (id: number) => {
     const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
+  const toggleEditMode = (id: number) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isEditing = !todo.isEditing;
+      }
+      return todo;
+    });
 
     setTodos(newTodos);
-
   };
-
 
   return (
     <div className='header'>
-      <span >Todoリスト with Typescript</span>
-    <div className="App">
+      <span className='title'>Todoリスト with Typescript</span>
+      <div className='back'>
+        <div className="App">
+          <div className='list'>
+            <div className='status'>
+              <Status />
+            </div>
 
-      
-      
-      <div className='list'>
-        
-        <div className='status'>
-        <Status />
-      </div>
-
-        <form onSubmit={(e) => handleSubmit(e)}>
-
-
-          <input
-            type="text"
-            onChange={(e) => handleChange(e)}
-            className='inputText'
-          />
-
-          <input type='submit' value="作成" className="submitButton" />
-
-         
-        </form>
-        {/* タスク設定が完了したら */}
-        <ul className='todoList'>
-          {todos.map((todo) => (
-            <li key={todo.id}>
-
+            <form onSubmit={(e) => handleSubmit(e)}>
               <input
                 type="text"
-                onChange={(e) => handleEdit(todo.id, e.target.value)}
+                onChange={(e) => handleChange(e)}
                 className='inputText'
-                value={todo.inputValue}
-                disabled={todo.checked}
+                value={inputValue}
               />
-              <input
-                type="checkbox"
-                checked={todo.checked}
-                onChange={() => handleChecked(todo.id, todo.checked)} />
-              <button onClick={() => handledelete(todo.id,)}>消</button>
-            </li>
-          ))}
-        </ul>
+              <input type='submit' value="作成" className="submitButton" />
+            </form>
 
+            {/* タスク設定が完了したら */}
+            <ul className='todoList'>
+
+
+              {todos.map((todo) => (
+                <li className='listtodo' key={todo.id}>
+                  <input
+                    type="checkbox"
+                    checked={todo.checked}
+                    onChange={() => handleChecked(todo.id)}
+                  />
+
+
+                  {todo.isEditing ? (
+                    <input
+                      type="text"
+                      onChange={(e) => handleEdit(todo.id, e.target.value)}
+                      className='inputText'
+                      value={todo.inputValue}
+                    />
+                  ) : (
+                    <span className={todo.checked ? 'strikethrough' : ''}>
+                      {todo.inputValue}
+                    </span>
+                  )}
+
+
+                  <button className='keep' onClick={() => toggleEditMode(todo.id)}>
+                    {todo.isEditing ? '保存' : '編集'}
+                  </button>
+
+                  <button className='delete' onClick={() => handleDelete(todo.id)}>消</button>
+                  <span className="timestamp"> <AccessTimeIcon style={{ verticalAlign: 'middle' }} />
+                    {todo.timestamp}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
-      <div className='side'>
-        <Side />
-
-      </div>
-
     </div>
-    
-    </div>
+
   );
-
-
-
 }
 
 export default App;
